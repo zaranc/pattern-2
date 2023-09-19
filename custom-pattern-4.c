f0f5c5715d52b358b11ca226cf25df9c9e6d73a4
Akash  Savaliya <130464977+zaranc@users.noreply.github.com>
2023-09-19 08:24:24 +0530

    Add files via upload

---
+++ custom-pattern-4.c
@@ -0,0 +1,29 @@
+/*
+1                 1
+1 2             2 1
+1 2 3         3 2 1
+1 2 3 4     4 3 2 1
+1 2 3 4 5 5 4 3 2 1 
+*/
+#include<stdio.h>
+int main()
+{
+	int i,j,k;
+	for(i=1;i<=5;i++)
+	{		
+		for(j=1;j<=i;j++)
+		{
+			printf("%d",j);
+		}
+		for(k=i;k<=4;k++)
+		{
+			printf("  ");
+		}
+		for(j=i;j>=1;j--)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+	
+}
